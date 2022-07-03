6e6deb2fcebaf428a4fbc18290e4a5a32844e127
xNtrow <xntrowdev@gmail.com>
2022-07-03 16:31:53 +0200

    Update import

--- src/main/java/net/luxclient/mixins/gui/MixinGuiIngame.java
+++ src/main/java/net/luxclient/mixins/gui/MixinGuiIngame.java
@@ -17,9 +17,4 @@ public class MixinGuiIngame {
         new Render2DEvent(partialTicks).call();
     }
 
-    @Inject(method = "renderGameOverlay", at = @At("RETURN"))
-    private void runTick (CallbackInfo info) {
-        LuxClient.getModuleManager().renderHud();
-    }
-
 }
